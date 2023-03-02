401e8dc9a71d181bc15c23bdc72d322102d90967
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-03-02 20:18:01 +0800

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const config = require('./config.json');
+const moment = require('moment-timezone');
+moment.tz.setDefault('Asia/Manila');
+let mt = moment();
+let time = `${mt}`;
 
-app.get('/', (req, res) => res.send(`${config.botName} is active!`));
-
+app.get('/', (req, res) => {
+  res.type('json').send(JSON.stringify({ name: config.botName, active: config.isActive, started: time }, null, 2) + '\n');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`${config.botName} is listening on port ${port}!`));
