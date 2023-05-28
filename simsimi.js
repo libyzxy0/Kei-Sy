d698b19ac4e42b3e5888ecf062f3b6840aadcde1
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2023-05-28 14:59:18 +0800

    Update simsimi.js

--- simsimi.js
+++ simsimi.js
@@ -2,12 +2,15 @@ const axios = require("axios");
 class Simsimi {
 	constructor(message) {
 		this.message = message;
-	}
+  } 
 	getresponse(callback) {
 		let a = axios.get(`https://api.simsimi.net/v2/?text=${this.message}&lc=ph&cf=false&name=Joyce`);
+    //ai.createChatCompletion(this.message, (resp) => {
+ //     callback(resp)
+//    })
 		a.then((response) => {
 			callback(response.data.success);
-        }) 
+    }) 
 	}
 }
 module.exports = { Simsimi }
